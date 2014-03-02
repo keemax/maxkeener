df75d50e28afd1a87e67ee9659b2b2a1dcb617fc
Max Keener <maxnkeener@gmail.com>
2014-03-02 07:13:27 +0000

    added alyssa page

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -8,7 +8,11 @@ import views.html.*;
 public class Application extends Controller {
 
     public static Result index() {
-        return ok(index.render("Your new application is ready."));
+        return ok(index.render("my name is Max"));
+    }
+
+    public static Result alyssa() {
+        return ok(alyssa.render());
     }
 
 }
