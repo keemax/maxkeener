0369be43997c7ee04ac2165cc6bbec0c8db2bf21
Max Keener <maxnkeener@gmail.com>
2014-03-03 23:02:31 -0800

    made navbar transluscent

--- public/javascripts/navbar.js
+++ public/javascripts/navbar.js
@@ -1,10 +1,8 @@
 function swapGithub() {
 	$("#github").hover(function() {
-		$(this).css("background-color", "rgb(220, 220, 220)");
 		$(this).find(".light").hide();
 		$(this).find(".dark").show();
 	}, function() {
-		$(this).css("background-color", "rgb(20, 20, 20)");
 		$(this).find(".dark").hide();
 		$(this).find(".light").show();
 	});
